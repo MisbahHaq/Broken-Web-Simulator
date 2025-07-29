1437089a6114f12bfb119dfaf67502d947dfe107
Misbah Haque <misbahmuneer2003@gmail.com>
2025-07-29 09:05:11 +0500

    Update

--- maps/lib/main.dart
+++ maps/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maps/Pages/map_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyPage());
   }
 }
