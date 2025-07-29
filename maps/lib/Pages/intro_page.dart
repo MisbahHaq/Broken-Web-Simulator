c8eaf9c6483d578200239cc510de36eba486ce9b
Misbah Haque <misbahmuneer2003@gmail.com>
2025-07-29 09:35:17 +0500

    Update

--- maps/lib/Pages/intro_page.dart
+++ maps/lib/Pages/intro_page.dart
@@ -10,6 +10,21 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 138, 60, 55),
+      body: Column(
+        children: [
+          // Shop Name
+
+          // Icon
+
+          // Title
+
+          // Subtitle
+
+          // Get Started Button
+        ],
+      ),
+    );
   }
 }
